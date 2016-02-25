ab1708a37cf1af305e237b39b2a18d6db2640849
Kinoko <daisuke_hasegawa@librastudio.jp>
2016-02-25 13:52:27 +0900

    Initial Commit

---
+++ ViperArchitecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViperArchitecture
+//
+//  Created by Libra Studio, Inc. on 2016/02/25.
+//  Copyright © 2016年 Libra Studio, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
